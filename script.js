324e489d24afbfce3c4f503e198ab52498882f9e
tomaskucera00 <99327920+tomaskucera00@users.noreply.github.com>
2022-02-10 22:59:16 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 /*Menu bar sticky*/
 window.addEventListener("scroll",function(){
-    var header = this.document.querySelector("header");
-    header.classList.toggle("sticky", window.scrollY > 0);
+  var header = this.document.querySelector("nav");
+  header.classList.toggle("sticky", window.scrollY > 0);
 })
 
 /*Posouvání mezi sekcemi*/
-$('header a').on('click',function(e){
-    if(this.hash !== '') {
-        e.preventDefault();
+$('nav a').on('click',function(e){
+  if(this.hash !== '') {
+      e.preventDefault();
 
-        const hash = this.hash;
+      const hash = this.hash;
 
-        $('html, body').animate({
-            scrollTop: $(hash).offset().top
-        }, 1200);
-    }
+      $('html, body').animate({
+          scrollTop: $(hash).offset().top
+      }, 1200);
+  }
 })
 
 /*swiper*/
@@ -41,5 +41,21 @@ var swiper = new Swiper(".mySwiper", {
     },
   });
 
-/*Contact us*/
 
+/*rozbaleni menu mobil*/
+
+const navSlide = () => {
+  const burger = document.querySelector('.burger');
+  const nav = document.querySelector('.nav-links');
+  const navLinks = document.querySelectorAll('.nav-links li');
+
+  burger.addEventListener('click', () => {
+    nav.classList.toggle('nav-active');
+  });
+
+  navLinks.forEach((link, index) => {
+    link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0,3}s`;
+  });
+}
+
+navSlide();
